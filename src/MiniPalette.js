5a84916b614ff7d036962cbcea6301d0876b4269
Ricardo Monterrosa <rickyantonio117@gmail.com>
2021-02-22 21:05:45 -0800

    Add Link to Palette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -45,7 +45,7 @@ const styles = {
 }
 
 function MiniPalette(props) {
-    const { classes, paletteName, emoji, colors } = props;
+    const { classes, paletteName, emoji, colors, handleClick } = props;
     const miniColorBoxes = colors.map(color => (
         <div 
             className={classes.miniColor}
@@ -57,7 +57,7 @@ function MiniPalette(props) {
     ));
 
     return (
-        <div className={classes.root}>
+        <div className={classes.root} onClick={handleClick}>
             <div className={classes.colors}>
                 {miniColorBoxes}
             </div>
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -35,10 +35,14 @@ const styles = {
 }
 
 class PaletteList extends Component {
-
+    goToPalette(id) {
+        this.props.history.push(`/palette/${id}`)
+    }
+    
     render() {
         const { palettes, classes } = this.props;
         return (
+            
             <div className={classes.root}>
                 <div className={classes.container}>
                     <nav className={classes.nav}>
@@ -46,7 +50,9 @@ class PaletteList extends Component {
                     </nav>
                     <div className={classes.palettes}>
                         { palettes.map(palette => (
-                                <MiniPalette {...palette} />
+
+                                    <MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)}/>
+                             
                             )) }
                     </div>
                 </div>
